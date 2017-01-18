bceea2627c9919bf8be9790ce6443b4e7c9a513f
Iegor Maiorov <maiorov.prog@gmail.com>
2017-01-18 23:21:31 +0200

    experimented with session

--- src/main/java/spring_boot/controllers/HelloController.java
+++ src/main/java/spring_boot/controllers/HelloController.java
@@ -2,13 +2,10 @@ package spring_boot.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class HelloController {
@@ -19,11 +16,27 @@ public class HelloController {
 	public String hello(HttpServletResponse response, @CookieValue(value = "foo", defaultValue = "whatever") String foo) {
 		log.info("foo is: " + foo);
 		response.addCookie(new Cookie("foo", "bar"));
-		return "{hello:world}";
+		return "{hello:world} >>> ";
 	}
 
 	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public String index() {
+	public String index(HttpSession httpSession) {
+		httpSession.setAttribute("intro", "introduction");
 		return "{greeting: hi there}";
+	}
+
+	@RequestMapping(path = "/session", method = RequestMethod.GET)
+	public String session(HttpSession httpSession) {
+		if (httpSession.getAttribute("intro") == null) {
+			return "{msg: session is empty}";
+		}
+
+		return "{msg: session is applied, value : " + httpSession.getAttribute("intro") + "}";
+	}
+
+
+	@ModelAttribute("intro")
+	public String createIntro() {
+		return "this is intro to sessions in spring mvc";
 	}
 }
